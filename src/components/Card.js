5e1a65fd6002de5f7143503ef0682327c8f28a7f
Hyun Don Moon <hyundonmoon@gmail.com>
2021-06-21 22:25:24 +0900

    Created Card component and container component to hold cards

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardWrapper from "../styles/Card";
+
+const Card = () => {
+  return (
+    <CardWrapper>
+      <div>Card Component</div>
+    </CardWrapper>
+  );
+};
+
+export default Card;
---
+++ src/styles/CardsContainer.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const CardContainer = styled.div``;
+
+export default CardContainer;
