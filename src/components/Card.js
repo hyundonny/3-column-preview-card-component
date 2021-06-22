795e7f352d56a03cf869858ef55af8e491e42835
Hyun Don Moon <hyundonmoon@gmail.com>
2021-06-22 19:01:15 +0900

    Modified logo alt attribute

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = ({ type }) => {
   return (
     <StyledCard bgColor={content.bgColor}>
       <div>
-        <img src={content.logo} alt={`logo of car`} />
+        <img src={content.logo} alt="logo of a car" />
       </div>
       <h3 className="card__title">{type}</h3>
       <p className="card__body">{content.body}</p>
